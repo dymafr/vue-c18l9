24f53ca17ca8586c3af3b8749a8a2aabb1fa0c39
dymafr <erwan1309@hotmail.com>
2022-04-05 13:05:13 +0200

    1 file modified

--- src/PageB.vue
+++ src/PageB.vue
@@ -1,23 +1,32 @@
 <template>
   <div class="card p-20 d-flex justify-content-center align-items-center">
     <h1>La page B</h1>
-    <div></div>
+    <div v-if="state.user">
+      <h2>{{ user.name }}</h2>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { reactive, watchEffect } from 'vue';
-import { useRoute, onBeforeRouteUpdate } from 'vue-router';
+import { useRoute } from 'vue-router';
 
-const state = reactive<{ user: any }>({ user: null });
+const state = reactive<{ user: any; users: any[] }>({
+  user: null,
+  users: null,
+});
 
 const route = useRoute();
 
 watchEffect(async () => {
+  state.users = await fetch('https://restapi.fr/api/usertest/').json();
+  console.log(state.users);
   if (route.params.userId) {
     state.user = await fetch(
       `https://restapi.fr/api/usertest/${route.params.userId}`
     ).json();
+  } else {
+    state.user = null;
   }
 });
 </script>
